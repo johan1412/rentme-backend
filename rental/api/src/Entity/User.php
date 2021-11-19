f9de7dba06e7cc9287191524086a3a5a93c97280
Abdellatif CHALAL <abdellatifchalal44@gmail.com>
2021-11-19 15:14:28 +0100

    add jwt auth

--- rental/api/src/Entity/User.php
+++ rental/api/src/Entity/User.php
@@ -3,18 +3,19 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 
 /**
- * @ApiResource
- * @ORM\Entity
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
  */
-class User
+class User implements UserInterface
 {
     /**
-     * @var int The entity Id
-     *
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
@@ -22,80 +23,150 @@ class User
     private $id;
 
     /**
-     * @var string First name
-     *
-     * @ORM\Column
-     * @Assert\NotBlank
+     * @ORM\Column(type="string", length=180, unique=true)
      */
-    public $firstName = '';
+    private $email;
 
     /**
-     * @var string Last name
-     *
-     * @ORM\Column
-     * @Assert\NotBlank
+     * @ORM\Column(type="json")
      */
-    public $lastName = '';
-
-     /**
-     * @var string Email
-     *
-     * @ORM\Column
-     * @Assert\NotBlank
-     */
-    public $email = '';
+    private $roles = [];
 
     /**
-     * @var string Adress
-     *
-     * @ORM\Column
-     * @Assert\NotBlank
+     * @var string The hashed password
+     * @ORM\Column(type="string")
      */
-    public $adress = '';
+    private $password;
 
     /**
-     * @var string Birth Date
-     *
-     * @ORM\Column
-     * @Assert\NotBlank
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
-    public $birthDate = '';
+    private $firstName;
 
     /**
-     * @var string Phone Number
-     *
-     * @ORM\Column
-     * @Assert\NotBlank
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
-    public $phoneNumber = '';
+    private $lastName;
 
-    public function getId(): int
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getFirstName(): int
-    {
-        return $this->firstName;
-    }
-
-    public function getLastName(): int
-    {
-        return $this->lastName;
-    }
-
-    public function getEmail(): int
+    public function getEmail(): ?string
     {
         return $this->email;
     }
 
-    public function getBirthDate(): int
+    public function setEmail(string $email): self
     {
-        return $this->birthDate;
+        $this->email = $email;
+
+        return $this;
     }
 
-    public function getPhoneNumber(): int
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
     {
-        return $this->phoneNumber;
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(?string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(?string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
     }
 }
