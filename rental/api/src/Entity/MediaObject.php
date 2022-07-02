62f1ba702b7aeb61ad9b97a4925e2dfed39459ac
Bouhadjar Amine <bouhadjarmedamine@gmail.com>
2022-07-03 01:56:36 +0200

    fix upload file

--- rental/api/src/Entity/MediaObject.php
+++ rental/api/src/Entity/MediaObject.php
@@ -7,7 +7,6 @@ use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Controller\CreateMediaObjectAction;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
