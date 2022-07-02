8b33989a65440e1c42a75c94082b3242051bb8f3
Bouhadjar Amine <bouhadjarmedamine@gmail.com>
2022-07-03 01:02:58 +0200

    add https

--- rental/api/src/Entity/MediaObject.php
+++ rental/api/src/Entity/MediaObject.php
@@ -15,7 +15,7 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
 /**
  * @ORM\Entity
  * @ApiResource(
- *     iri="http://schema.org/MediaObject",
+ *     iri="https://schema.org/MediaObject",
  *     normalizationContext={
  *         "groups"={"media_object_read"}
  *     },
@@ -64,7 +64,7 @@ class MediaObject
     /**
      * @var string|null
      *
-     * @ApiProperty(iri="http://schema.org/contentUrl")
+     * @ApiProperty(iri="https://schema.org/contentUrl")
      * @Groups({"media_object_read"})
      */
     public $contentUrl;
